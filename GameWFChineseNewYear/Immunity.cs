27314687a54f9868e567293c0109249d8820c257
Alexandr <60702539+Alexandrjob@users.noreply.github.com>
2020-03-16 22:49:21 +1000

    Убрал "0"

--- GameWFChineseNewYear/Immunity.cs
+++ GameWFChineseNewYear/Immunity.cs
@@ -17,7 +17,7 @@
                 {
                     percentageOfImmunity = 0;
                 }
-                else if (value > 1000)
+                else if (value > 100)
                 {
                     percentageOfImmunity = 100;
                 }
